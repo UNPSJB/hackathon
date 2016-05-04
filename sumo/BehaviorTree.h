2d9ce7afa10929499f94a5b4192d5a8ef684c81b
Diego van Haaster <diegomvh@gmail.com>
2016-05-04 13:05:10 -0300

    SumoFruta

---
+++ sumo/BehaviorTree.h
@@ -0,0 +1,165 @@
+#include <StandardCplusplus.h>
+#include <utility.h>
+#include <system_configuration.h>
+#include <unwind-cxx.h>
+#include <vector>
+#include <iterator>
+
+#ifndef BEHAVIORTREE_H
+#define BEHAVIORTREE_H
+
+enum Estado
+{
+  BH_INVALIDO,
+	BH_EXITO,
+	BH_FALLO,
+	BH_CORRIENDO,
+};
+
+typedef struct Memoria {unsigned long *sensores, *comportamientos;} Memoria;
+
+typedef Estado (*Tarea)(Memoria);
+typedef Estado (*Ver)(int, Memoria);
+
+class Nodo
+{
+public:
+  Nodo(const char* nombre)
+  :	nombre(nombre), estado(BH_INVALIDO)
+  {
+  }
+
+	virtual ~Nodo() {}
+
+  virtual Estado hacer(Memoria memoria) = 0;
+  virtual void entrar() {}
+	virtual void iniciar() {}
+	Estado tick(Memoria memoria) {
+    entrar();
+    if (estado != BH_CORRIENDO)
+      iniciar();
+
+    estado = hacer(memoria);
+
+    if (estado != BH_CORRIENDO)
+      finalizar(estado);
+    salir(estado);
+    return estado;
+  }
+  virtual void finalizar(Estado) {}
+  virtual void salir(Estado) {}
+
+protected:
+  Estado estado;
+  const char* nombre;
+};
+
+class Accion : public Nodo
+{
+public:
+  Accion(const char* nombre, Tarea tarea)
+  :	Nodo(nombre), tarea(tarea)
+  {
+  }
+
+	virtual ~Accion() {}
+
+  virtual Estado hacer(Memoria memoria) { return tarea(memoria); }
+
+protected:
+	Tarea tarea;
+};
+
+class Mirar : public Nodo
+{
+public:
+  Mirar(const char* nombre, int indice, Ver ver)
+  :	Nodo(nombre), indice(indice), ver(ver)
+  {
+  }
+
+	virtual ~Mirar() {}
+
+  virtual Estado hacer(Memoria memoria) { return ver(indice, memoria); }
+
+protected:
+	int indice;
+  Ver ver;
+};
+
+class Compuesto : public Nodo
+{
+public:
+  Compuesto(const char* nombre) :	Nodo(nombre) { }
+  void aprender(Nodo* nodo) {
+    nodos.push_back(nodo);
+  }
+protected:
+  typedef std::vector<Nodo*> Nodos;
+  Nodos nodos;
+};
+
+class Secuencia : public Compuesto {
+public:
+  Secuencia(const char* nombre) :	Compuesto(nombre) { }
+
+  virtual void iniciar() {
+    actual = nodos.begin();
+  }
+
+  virtual Estado hacer(Memoria memoria)
+  {
+    // Ejecutar hasta que un comportamiento diga que esta corriendo
+    for (;;) {
+      Estado e = (*actual)->tick(memoria);
+      // Si un comportamiento esta corriendo o fallo hacer lo mismo
+      if (e != BH_EXITO) {
+        return e;
+      }
+
+      // Hit the end of the array, job done!
+      if (++actual == nodos.end()) {
+        return BH_EXITO;
+      }
+    }
+  }
+  Nodos::iterator actual;
+};
+
+class Selector : public Compuesto {
+public:
+  Selector(const char* nombre) :	Compuesto(nombre) { }
+
+  virtual void iniciar() {
+    actual = nodos.begin();
+  }
+
+  virtual Estado hacer(Memoria memoria) {
+    // Ejecutar hasta que un comportamiento diga que esta corriendo
+  	for (;;) {
+      Estado e = (*actual)->tick(memoria);
+
+      // Si un comportamiento esta corriendo o fue exitoso hacer lo mismo
+      if (e != BH_FALLO) {
+          return e;
+      }
+
+      // Hit the end of the array, it didn't end well...
+      if (++actual == nodos.end()) {
+          return BH_FALLO;
+      }
+    }
+  }
+  Nodos::iterator actual;
+};
+
+class Comportamiento : public Selector {
+public:
+  Comportamiento(const char* nombre) :
+    Selector(nombre) { }
+
+  Estado actuar(Memoria memoria) {
+    return tick(memoria);
+  }
+};
+#endif
--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -3,35 +3,25 @@
 #include <system_configuration.h>
 #include <unwind-cxx.h>
 #include <DCMotor.h>
-#include <FuzzyRule.h>
-#include <FuzzyComposition.h>
-#include <Fuzzy.h>
-#include <FuzzyRuleConsequent.h>
-#include <FuzzyOutput.h>
-#include <FuzzyInput.h>
-#include <FuzzyIO.h>
-#include <FuzzySet.h>
-#include <FuzzyRuleAntecedent.h>
 #include "BehaviorTree.h"
 
 #define izquierdaIR A0
 #define derechaIR A1
 #define pingPin A5
-#define IR_MIN 430;
-#define IR_MAX 990;
-#define IR_CORTE (IR_MIN + IR_MAX) / 2;
-#define MOTOR_MIN 10;
-#define MOTOR_MAX 100;
+#define IR_MIN = 430;
+#define IR_MAX = 990;
+#define IR_CORTE = (IR_MIN + IR_MAX) / 2;
+#define MOTOR_MIN = 10;
+#define MOTOR_MAX = 100;
 
 DCMotor motor1(M0_EN, M0_D0, M0_D1);
 DCMotor motor2(M1_EN, M1_D0, M1_D1);
 
 // Instanciando un objeto de biblioteca
-Fuzzy* fuzzy = new Fuzzy();
-Comportamiento* comportamiento = new Comportamiento("N6 Seguidor de lineas");
+Comportamiento* comportamiento = new Comportamiento("N6 Sumo");
 
-unsigned long sensores[10] = {0};
-unsigned long comportamientos[10] = {0};
+unsigned long sensores[3] = {0};
+unsigned long comportamientos[100] = {0};
 
 Memoria memoria = {sensores, comportamientos};
 
@@ -43,42 +33,9 @@ void setup(){
   motor1.setClockwise(false);
   motor2.setClockwise(false);
 
-  FuzzyInput* intencidad = new FuzzyInput(1);
-  FuzzySet* baja = new FuzzySet(0, 30, 30, 50);
-  intencidad->addFuzzySet(baja);
-  FuzzySet* alta = new FuzzySet(40, 60, 60, 80);
-  intencidad->addFuzzySet(alta);
-
-  fuzzy->addFuzzyInput(intencidad);
-
-  FuzzyOutput* velocidad = new FuzzyOutput(1);
-  FuzzySet* lenta = new FuzzySet(0, 10, 10, 20);
-  velocidad->addFuzzySet(lenta);
-  FuzzySet* rapida = new FuzzySet(10, 20, 30, 40);
-  velocidad->addFuzzySet(rapida);
-
-  fuzzy->addFuzzyOutput(velocidad);
-
-  // Si la intensidad es baja => la velocidad es rapida
-  FuzzyRuleAntecedent* ifIntensidadBaja = new FuzzyRuleAntecedent();
-  ifIntensidadBaja->joinSingle(baja);
-  FuzzyRuleConsequent* thenVelocidadRapida = new FuzzyRuleConsequent();
-  thenVelocidadRapida->addOutput(rapida);
-  // La regla
-  FuzzyRule* fuzzyRegla01 = new FuzzyRule(1, ifIntensidadBaja, thenVelocidadRapida);
-  fuzzy->addFuzzyRule(fuzzyRegla01);
-
-  // Si la intensidad es alta => la velocidad es baja
-  FuzzyRuleAntecedent* ifIntensidadAlta = new FuzzyRuleAntecedent();
-  ifIntensidadAlta->joinSingle(alta);
-  FuzzyRuleConsequent* thenVelocidadLenta = new FuzzyRuleConsequent();
-  thenVelocidadLenta->addOutput(lenta);
-  // La regla
-  FuzzyRule* fuzzyRegla02 = new FuzzyRule(3, ifIntensidadAlta, thenVelocidadLenta);
-  fuzzy->addFuzzyRule(fuzzyRegla02);
-
   // Acciones del comportamiento
   Accion* a_avanzar = new Accion("Avanzar", f_avanzar);
+  Accion* a_evitar = new Accion("Evitar", f_evitar);
   Accion* a_girar = new Accion("Rotar", f_girar);
   Accion* a_retroceder = new Accion("Rotar", f_retroceder);
   Mirar* m_s0b = new Mirar("S0 blanco", 0, f_blanco);
@@ -87,37 +44,29 @@ void setup(){
   Mirar* m_s1n = new Mirar("S1 negro", 1, f_negro);
 
   // Comportamientos
-  // Seguir la linea
-  // seguir := blanco y blanco => avanzar
-  Secuencia* c_seguir = new Secuencia("Seguir linea");
-  c_seguir->aprender(m_s0b);
-  c_seguir->aprender(m_s1b);
-  c_seguir->aprender(a_avanzar);
-  comportamiento->aprender(c_seguir);
+  // Evitar la linea
+  // evitar linea := blanco o blanco => evitar
+  Secuencia* c_evitar = new Secuencia("Evitar linea");
+  Selector* m_blanco = new Selector("Mirar blanco");
+  c_blanco->aprender(m_s0b);
+  c_blanco->aprender(m_s1b);
+  c_evitar->aprender(m_blanco);
+  c_evitar->aprender(a_evitar);
+  comportamiento->aprender(c_evitar);
 
-  // Girar derecha
-  // girar derecha := negro y blanco => girar
-  Secuencia* c_girar_der = new Secuencia("Girar derecha");
-  c_girar_der->aprender(m_s0n);
-  c_girar_der->aprender(m_s1b);
-  c_girar_der->aprender(a_girar);
-  comportamiento->aprender(c_girar_der);
+  // Buscar oponente
+  // buscar oponente := !oponente => buscar
+  Secuencia* c_buscar = new Secuencia("Buscar oponente");
+  c_girar_der->aprender(m_oponente);  //Negar esto o hacer algo
+  c_girar_der->aprender(a_buscar);
+  comportamiento->aprender(c_buscar);
 
-  // Girar izquierda
-  // girar izquierda := blanco y negro => girar
-  Secuencia* c_girar_izq = new Secuencia("Girar izquierda");
-  c_girar_izq->aprender(m_s0b);
-  c_girar_izq->aprender(m_s1n);
-  c_girar_izq->aprender(a_girar);
-  comportamiento->aprender(c_girar_izq);
-
-  // Retroceder
-  // retroceder := negro y negro => retroceder
-  Secuencia* c_retroceder = new Secuencia("Retroceder");
-  c_retroceder->aprender(m_s0n);
-  c_retroceder->aprender(m_s1n);
-  c_retroceder->aprender(a_retroceder);
-  comportamiento->aprender(c_retroceder);
+  // Atacar
+  // atacar := oponente => avanzar
+  Secuencia* c_atacar = new Secuencia("Atacar");
+  c_girar_izq->aprender(m_oponente);
+  c_girar_izq->aprender(a_avanzar);
+  comportamiento->aprender(c_atacar);
 
   Serial.println(" === END SETUP ===");
 }
@@ -148,8 +97,7 @@ Estado f_avanzar(Memoria memoria) {
   Serial.println(MOTOR_MAX);
   //motor1.setSpeed(MOTOR_MAX);
   //motor2.setSpeed(MOTOR_MAX);
-  return BH_EXITO;
-  //return BH_CORRIENDO;
+  return BH_CORRIENDO;
 }
 
 Estado f_retroceder(Memoria memoria) {
@@ -159,11 +107,10 @@ Estado f_retroceder(Memoria memoria) {
   Serial.println(MOTOR_MAX);
   //motor1.setSpeed(-MOTOR_MAX);
   //motor2.setSpeed(-MOTOR_MAX);
-  return BH_EXITO;
-  //return BH_CORRIENDO;
+  return BH_CORRIENDO;
 }
 
-Estado f_girar(Memoria memoria) {
+Estado f_evitar(Memoria memoria) {
   fuzzy->setInput(0, memoria.sensores[0]);
   fuzzy->setInput(1, memoria.sensores[1]);
   fuzzy->fuzzify();
@@ -175,8 +122,7 @@ Estado f_girar(Memoria memoria) {
   Serial.println(m2_speed);
   //motor1.setSpeed(m1_speed);
   //motor2.setSpeed(m2_speed);
-  return BH_EXITO;
-  //return BH_CORRIENDO;
+  return BH_CORRIENDO;
 }
 
 long leerDistancia(int pingPin)
